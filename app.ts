26ba511f4fe1e49009ff27bcaab67a6a91c11ccc
Nelson M. Rios <nelsonmatias9@hotmail.com>
2024-03-30 18:16:04 -0300

    Update CORS origin to include protocol

--- app.ts
+++ app.ts
@@ -21,7 +21,7 @@ const server = Bun.serve({
         },
         {
           headers: {
-            "Access-Control-Allow-Origin": "www.tradingview.com",
+            "Access-Control-Allow-Origin": "https://www.tradingview.com",
             "Access-Control-Allow-Methods": "GET, OPTIONS",
           },
         },
