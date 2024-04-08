67c72a7b89c8ef495342c2b86391d2db18f8fd6a
Nelson M. Rios <nelsonmatias9@hotmail.com>
2024-04-08 12:19:37 -0300

    Add ping route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,8 @@ import { Elysia } from "elysia";
 import { symbolsRoute } from "./symbols.route";
 
 export const routes = (app: Elysia) => {
-  app.group("/symbols_list", symbolsRoute);
-
+  app.group("/symbols_list", symbolsRoute).get("/ping", () => {
+    return { message: "👋" };
+  });
   return Promise.resolve(app);
 };
